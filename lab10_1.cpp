bb8de4f2420644a2e8db4b0be4a514aaa9f26245
phongsathorn <phongsathorn_nil@cmu.ac.th>
2022-12-27 15:56:43 +0700

    lab10 commit

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,14 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double initial, interest, amount;
 	cout << "Enter initial loan: ";
+	cin >> initial;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> amount;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,41 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+
+	
+	
+		double balance = initial;
+		int year=1;
+
+		while(balance > 0.00)
+		{
+			cout << fixed << setprecision(2); 
+
+			cout << setw(13) << left << year; 
+
+			cout << setw(13) << left << balance;
+
+			double x;
+			x = (interest*balance)/100;
+			cout << setw(13) << left << x;
+			
+
+			double total;
+			total = balance+x;
+			cout << setw(13) << left << total;
+			
+
+			if(amount > total)
+				amount = total;
+			cout << setw(13) << left << amount;
+
+			balance = total - amount;
+			cout << setw(13) << left << balance;
+			cout << "\n";	
+			
+			year++;
+		}
+	
 	
 	return 0;
 }
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 //Write the function printO() here
 
+void printO(int N, int M)
+{
+	if(N <= 0 or M <= 0)
+	{
+		cout << "Invalid input";
+	}else{
+		for(int i = 0; i < N; i++){
+			for(int x = 0; x < M; x++)
+				cout << "O";
+			cout << "\n";
+		}
+	}
+
+}
+
+
 int main(){
 	
 	printO(2,2);
